fbde03a0fdc6d029ef4f7b39942eaed0a1a99294
Maksim Brutsky <74231817+Triaster@users.noreply.github.com>
2022-12-21 16:19:36 +0300

    Add files via upload

---
+++ result-info/pdf.js
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ru">
+<head>
+  <meta charset="utf-8">
+  <title>Регистрация на РТ</title>
+  <meta name="keywords" content="регистрация на рт, зарегистрироваться на репетиционное тестирование, РТ, личный кабинет РТ, личный кабинет РИКЗ">
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <meta http-equiv="cache-control" content="no-cache">
+  <meta http-equiv="expires" content="0">
+<link rel="stylesheet" href="styles.37b7070aa37ddab101d9.css"></head>
+<body>
+  <app-root></app-root>
+<script type="text/javascript" src="runtime.753ca11b96a0d85c940a.js"></script><script type="text/javascript" src="polyfills.20ba6e672b79f40e8ced.js"></script><script type="text/javascript" src="scripts.d674e8d9516d973e2da6.js"></script><script type="text/javascript" src="vendor.ca204015afc89fbb5c9e.js"></script><script type="text/javascript" src="main.13fb9339f3a9dd881633.js"></script></body>
+
+
+</html>
